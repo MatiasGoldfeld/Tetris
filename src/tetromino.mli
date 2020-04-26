12719f089a75929d44c2385122f626b610ed01db
Ojmatte <ojm23@cofnell.edu>
2020-04-26 12:13:36 -0500

    Weird errors

--- src/tetromino.mli
+++ src/tetromino.mli
@@ -13,15 +13,6 @@ val defaults : t list
 (** [size t] is the bounding box of tetromino t. *)
 val size : t -> int
 
-<<<<<<< HEAD
-(** [color tet] is the color option of [tet] *)
-val color : t -> (int * int * int) option
-
-(** [max_size] is the maximum size of the default tetrominoes. *)
-val max_size : int
-
-(** [value t rot x y] is a color option of the tetromino where [rot] is the
-=======
 (** [max_size] is the maximum size of the default tetrominoes. *)
 val max_size : int
 
@@ -29,7 +20,6 @@ val max_size : int
 val color : t -> (int * int * int) option
 
 (** [value tet rot x y] is a color option of the tetromino where [rot] is the
->>>>>>> b32271c4cb7ef838fa6251e10046665d622b20db
     rotation from 0 to 3, [x] is the x coordinate, and [y] is the y coordinate. 
 *)
 val value : t -> int -> int -> int -> (int * int * int) option
