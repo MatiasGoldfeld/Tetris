79981468b5f5ffd739d79a36cca45d4ea4b7d171
Matias Goldfeld <mg2277@cornell.edu>
2020-05-10 23:32:47 -0400

    cleaned up main

--- src/main.ml
+++ src/main.ml
@@ -2,41 +2,6 @@ open Tsdl
 open Lwt.Infix
 open Ppx_lwt
 
-module Local = Game.Make (State.Local)
-module Client = Game.Make (Remote.Client)
-module Server = Game.Make (Remote.Server)
-
-let menu_controls = [
-  (Sdl.K.escape, Game.MMenu);
-  (Sdl.K.left,   Game.MLeft);
-  (Sdl.K.right,  Game.MRight);
-  (Sdl.K.up,     Game.MUp);
-  (Sdl.K.down,   Game.MDown);
-  (Sdl.K.return, Game.MEnter);
-]
-
-let game_controls = [
-  (Sdl.K.escape, Game.GMenu);
-  (Sdl.K.left,   Game.GLeft);
-  (Sdl.K.right,  Game.GRight);
-  (Sdl.K.up,     Game.GCW);
-  (Sdl.K.z,      Game.GCCW);
-  (Sdl.K.x,      Game.GCW);
-  (Sdl.K.down,   Game.GSoft);
-  (Sdl.K.space,  Game.GHard);
-  (Sdl.K.c,      Game.GHold);
-]
-
-let addr = Lwt_unix.ADDR_INET (Unix.inet_addr_loopback, 9000)
-
-let test_server audio graphics =
-  let state = Remote.create_server "" addr in
-  Server.init audio graphics menu_controls game_controls state
-
-let test_client audio graphics =
-  let state = Remote.create_client "" addr in
-  Client.init audio graphics menu_controls game_controls state
-
 (** [main] loads SDL components and starts up the main menu. *)
 let main : unit Lwt.t =
   match Sdl.init Sdl.Init.(events + timer) with
@@ -45,7 +10,6 @@ let main : unit Lwt.t =
     let audio = Audio.init "./resources/audio/" in
     let graphics = Graphics.init false "./resources/"  in
     Audio.adjust_music audio 0.05;
-    (* let%lwt () = test_server audio graphics in *)
     let%lwt () = Menu.init audio graphics
         [("Multiplayer", "checkbox", 
           (fun m -> Menu_state.toggle_multiplayer m )); 
